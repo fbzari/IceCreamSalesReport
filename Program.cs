b7c967187b28e7c137a2f743b28571109746ee37
Ariyanayagan <ariya.nayagan@technopalette.solutions>
2026-05-08 16:46:47 +0530

    Update Q/A answers

--- Program.cs
+++ Program.cs
@@ -1,6 +1,8 @@
 using IceCreamSales.Endpoints;
 using IceCreamSales.Services;
 
+/// This Repo Contains the Sales Report Assessment
+/// 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -26,3 +28,11 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 app.MapReportEndpoints();
 app.Run();
+
+///1) What was the most complex part of the assignment for you personally and why?
+///    ans : For Generate Report as pdf for crucial part. integrate separate razor 
+///    engine in this minimal API project
+///2) Describe a bug you expect to hit while implementing this and how you would debug it.
+/// ans : a bug that was facing while generate PDF using razor html. and generate pdf using thirt party library.
+///3) Does your solution handle larger data sets without any performance implications?
+/// ans : yes we have separate csv file. you can use large data also.
